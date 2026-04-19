774bd2a6c6a1b458bb442dee2f658d34a3385a55
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-04-19 09:21:25 -0600

    Crear setting.js

---
+++ setting.js
@@ -0,0 +1,19 @@
+global.namebot = 'Roky Bot'
+global.vs = '2.0.0'
+global.prefix = '.'
+global.libreria = 'Baileys Multi Device'
+
+global.dev = '𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔'
+global.etiqueta = '𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔'
+
+// JIDs completos ya que aún falta hacer que pase de jid a JIDS
+global.owner = [
+  ['5214183357841', 'Fernando', true],
+  ['5216631079388', 'neykoor', true]
+]
+
+// Base de datos temporal
+global.db = { data: { users: {} } }
+global.plugins = {}
+
+console.log('✅ Configuración global cargada')
