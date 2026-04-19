35a7ce9aa52195729dcf05349100ac02617e7a7d
andreslopez711913-art <andreslopez711913@gmail.com>
2026-04-19 15:14:11 -0500

    Update bot names and developer information

--- setting.js
+++ setting.js
@@ -1,15 +1,15 @@
-global.namebot = 'Roky Bot'
+global.namebot = 'Rocky Bot'
 global.vs = '2.0.0'
 global.prefix = '.'
 global.libreria = 'Baileys Multi Device'
 
-global.dev = '𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔'
+global.dev = 'Moises'
 global.etiqueta = '𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔'
 
 // JIDs completos ya que aún falta hacer que pase de jid a JIDS
 global.owner = [
   ['5214183357841', 'Fernando', true],
-  ['5216631079388', 'neykoor', true]
+  ['593998391403', 'Moises', true]
 ]
 
 // Base de datos temporal
